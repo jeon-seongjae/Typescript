48f908eeac17d6906df2a607c49069c181a460da
jeon-seongjae <tjdwoek62@google.com>
2021-08-02 22:47:19 +0900

    monday clear

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-interface Human {
-    name : string;
-    age: number;
-    gender: string;
+// interface Human {
+//     name : string;
+//     age: number;
+//     gender: string;
+// }
+
+// const person = {
+//     name : "jeon",
+//     age : 27,
+//     gender: "male"
+// };
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name : "jeon",
-    age : 27,
-    gender: "male"
-};
+const lynn = new Human("jeon", 27, "male");
+
+
 
 const sayHi = (person: Human):string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
