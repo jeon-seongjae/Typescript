8f8a1054078d1f7ac11f8f02b296e52ae70bb7f6
jeon-seongjae <tjdwoek62@google.com>
2021-08-03 22:22:41 +0900

    tue clear

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,47 @@
 //     gender: "male"
 // };
 
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender:string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+// class Human {
+//     public name: string;
+//     public age: number;
+//     public gender: string;
+//     constructor(name: string, age:number, gender:string) {
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+// }
+
+// const lynn = new Human("jeon", 27, "male");
+
+
+
+// const sayHi = (person: Human):string => {
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+// };
+
+// console.log(sayHi(lynn));
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp:number;
+
+    constructor(index:number, hash:string, previousHash:string, data:string, timestamp: number){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const lynn = new Human("jeon", 27, "male");
+const genesisBlock:Block = new Block(0, "2020202020", "", "hello", 123456);
 
+let blockchain: [Block] = [genesisBlock];
 
-
-const sayHi = (person: Human):string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
